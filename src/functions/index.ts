972f76bca1d2552b04403ee9f773ba5798c2b78f
Flo <florent.vouin@gmail.com>
2023-05-26 09:22:36 +0200

    ✨ Implement ProjectGetArtefactDestination for npm packages

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,10 +1,12 @@
 import { ModuleRegistrationContext } from '@causa/workspace';
 import { EventTopicGenerateCodeForTypeScriptAndJsonEvents } from './event-topic-generate-json.js';
+import { ProjectGetArtefactDestinationForNpmPackage } from './project-get-artefact-destination-npm-package.js';
 import { ProjectReadVersionForJavascript } from './project-read-version-javascript.js';
 
 export function registerFunctions(context: ModuleRegistrationContext) {
   context.registerFunctionImplementations(
     EventTopicGenerateCodeForTypeScriptAndJsonEvents,
+    ProjectGetArtefactDestinationForNpmPackage,
     ProjectReadVersionForJavascript,
   );
 }
---
+++ src/functions/project-get-artefact-destination-npm-package.ts
@@ -0,0 +1,26 @@
+import { WorkspaceContext } from '@causa/workspace';
+import { ProjectGetArtefactDestination } from '@causa/workspace-core';
+import {
+  makeNpmPackageArtefactDestination,
+  readNpmPackageFile,
+} from '../utils.js';
+
+/**
+ * Implements the {@link ProjectGetArtefactDestination} function for an npm package.
+ * This returns a normalized destination based on the package name and the provided tag: `<package-name>@<tag>`.
+ */
+export class ProjectGetArtefactDestinationForNpmPackage extends ProjectGetArtefactDestination {
+  async _call(context: WorkspaceContext): Promise<string> {
+    const projectPath = context.getProjectPathOrThrow();
+    const packageInfo = await readNpmPackageFile(projectPath);
+    return makeNpmPackageArtefactDestination(packageInfo, this.tag);
+  }
+
+  _supports(context: WorkspaceContext): boolean {
+    return (
+      ['javascript', 'typescript'].includes(
+        context.get('project.language') ?? '',
+      ) && context.get('project.type') === 'package'
+    );
+  }
+}
