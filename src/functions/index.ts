491df587571853df34f4e590938554fff5d4465e
Flo <florent.vouin@gmail.com>
2023-05-26 12:29:40 +0200

    ✨ Implement ProjectBuildArtefact for TypeScript packages

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,11 +1,13 @@
 import { ModuleRegistrationContext } from '@causa/workspace';
 import { EventTopicGenerateCodeForTypeScriptAndJsonEvents } from './event-topic-generate-json.js';
+import { ProjectBuildArtefactForTypeScriptPackage } from './project-build-artefact-ts-package.js';
 import { ProjectGetArtefactDestinationForNpmPackage } from './project-get-artefact-destination-npm-package.js';
 import { ProjectReadVersionForJavascript } from './project-read-version-javascript.js';
 
 export function registerFunctions(context: ModuleRegistrationContext) {
   context.registerFunctionImplementations(
     EventTopicGenerateCodeForTypeScriptAndJsonEvents,
+    ProjectBuildArtefactForTypeScriptPackage,
     ProjectGetArtefactDestinationForNpmPackage,
     ProjectReadVersionForJavascript,
   );
---
+++ src/functions/project-build-artefact-ts-package.spec.ts
@@ -0,0 +1,84 @@
+import { WorkspaceContext } from '@causa/workspace';
+import { ProjectBuildArtefact } from '@causa/workspace-core';
+import {
+  InvalidFunctionArgumentError,
+  NoImplementationFoundError,
+} from '@causa/workspace/function-registry';
+import { createContext } from '@causa/workspace/testing';
+import { jest } from '@jest/globals';
+import 'jest-extended';
+import { NpmService } from '../services/index.js';
+import { ProjectBuildArtefactForTypeScriptPackage } from './project-build-artefact-ts-package.js';
+
+describe('ProjectBuildArtefactForTypeScriptPackage', () => {
+  let context: WorkspaceContext;
+  let npmService: NpmService;
+
+  beforeEach(() => {
+    ({ context } = createContext({
+      configuration: {
+        workspace: { name: '🏷️' },
+        project: {
+          name: 'my-project',
+          type: 'package',
+          language: 'typescript',
+        },
+      },
+      functions: [ProjectBuildArtefactForTypeScriptPackage],
+    }));
+    npmService = context.service(NpmService);
+  });
+
+  it('should not support projects that are not written in TypeScript', () => {
+    ({ context } = createContext({
+      configuration: {
+        workspace: { name: '🏷️' },
+        project: { name: 'my-project', type: 'package', language: 'ruby' },
+      },
+      functions: [ProjectBuildArtefactForTypeScriptPackage],
+    }));
+    npmService = context.service(NpmService);
+
+    expect(() => context.call(ProjectBuildArtefact, {})).toThrow(
+      NoImplementationFoundError,
+    );
+  });
+
+  it('should not support projects that are not packages', () => {
+    ({ context } = createContext({
+      configuration: {
+        workspace: { name: '🏷️' },
+        project: {
+          name: 'my-project',
+          type: 'serviceContainer',
+          language: 'typescript',
+        },
+      },
+      functions: [ProjectBuildArtefactForTypeScriptPackage],
+    }));
+    npmService = context.service(NpmService);
+
+    expect(() => context.call(ProjectBuildArtefact, {})).toThrow(
+      NoImplementationFoundError,
+    );
+  });
+
+  it('should not allow the artefact option', async () => {
+    const actualPromise = context.call(ProjectBuildArtefact, {
+      artefact: '📦',
+    });
+
+    await expect(actualPromise).rejects.toThrow(InvalidFunctionArgumentError);
+  });
+
+  it('should run the build script', async () => {
+    jest.spyOn(npmService, 'build').mockResolvedValueOnce();
+
+    const actualArtefact = await context.call(ProjectBuildArtefact, {});
+
+    expect(actualArtefact).toEqual(context.projectPath);
+    expect(npmService.build).toHaveBeenCalledOnceWith({
+      workingDirectory: context.projectPath,
+    });
+  });
+});
