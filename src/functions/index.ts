78bd11db83de796e2562a560c4fc44e19bffb717
Flo <florent.vouin@gmail.com>
2023-06-01 07:30:56 +0200

    ✨ Implement ProjectBuildArtefact for TypeScript serverless functions

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,6 +2,7 @@ import { ModuleRegistrationContext } from '@causa/workspace';
 import { EventTopicGenerateCodeForTypeScriptAndJsonEvents } from './event-topic-generate-json.js';
 import { ProjectBuildArtefactForServiceContainer } from './project-build-artefact-service-container.js';
 import { ProjectBuildArtefactForTypeScriptPackage } from './project-build-artefact-ts-package.js';
+import { ProjectBuildArtefactForTypeScriptServerlessFunctions } from './project-build-artefact-ts-serverless-functions.js';
 import { ProjectGetArtefactDestinationForNpmPackage } from './project-get-artefact-destination-npm-package.js';
 import { ProjectPushArtefactForNpmPackage } from './project-push-artefact-npm-package.js';
 import { ProjectReadVersionForJavascript } from './project-read-version-javascript.js';
@@ -11,6 +12,7 @@ export function registerFunctions(context: ModuleRegistrationContext) {
     EventTopicGenerateCodeForTypeScriptAndJsonEvents,
     ProjectBuildArtefactForServiceContainer,
     ProjectBuildArtefactForTypeScriptPackage,
+    ProjectBuildArtefactForTypeScriptServerlessFunctions,
     ProjectGetArtefactDestinationForNpmPackage,
     ProjectPushArtefactForNpmPackage,
     ProjectReadVersionForJavascript,
