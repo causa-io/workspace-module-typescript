ff9671f2895c1c5de60ec7862cac40bece1a5da6
Flo <florent.vouin@gmail.com>
2023-06-01 09:21:35 +0200

    🐛 Ensure the distroless image matches the major Node version

--- src/functions/project-build-artefact-ts-service-container.ts
+++ src/functions/project-build-artefact-ts-service-container.ts
@@ -5,20 +5,29 @@ import {
   ServiceContainerConfiguration,
 } from '@causa/workspace-core';
 import { resolve } from 'path';
+import { major } from 'semver';
 import { fileURLToPath } from 'url';
 import * as uuid from 'uuid';
 import { TypeScriptConfiguration } from '../index.js';
 
+/**
+ * The default Dockerfile for TypeScript service containers.
+ */
 const DOCKER_FILE = fileURLToPath(
   new URL('../assets/Dockerfile-typescript-service-container', import.meta.url),
 );
+
+/**
+ * The default Node.js major version when `javascript.node.version` is set to `latest`.
+ */
+const DEFAULT_NODE_MAJOR_VERSION = '20';
 
 /**
  * Implements the {@link ProjectBuildArtefact} function for a TypeScript service container.
  * This uses the Docker CLI to build the image and return its tag, which can be specified in
  * {@link ProjectBuildArtefact.artefact}. Otherwise, a UUID will be generated as the tag.
  * The default Dockerfile can be overridden using the `typescript.serviceContainerDockerfile` configuration.
- * `NODE_VERSION` and `NPM_VERSION` are automatically passed as build arguments, obtained from the
+ * `NODE_VERSION`, `NODE_MAJOR_VERSION` and `NPM_VERSION` are automatically passed as build arguments, obtained from the
  * `javascript.node.version` and `javascript.npm.version` configurations.
  */
 export class ProjectBuildArtefactForTypeScriptServiceContainer extends ProjectBuildArtefact {
@@ -40,8 +49,15 @@ export class ProjectBuildArtefactForTypeScriptServiceContainer extends ProjectBu
     const file = confFile ? resolve(context.rootPath, confFile) : DOCKER_FILE;
     const platform = serviceContainerConf.get('serviceContainer.architecture');
     const imageName = this.artefact ?? uuid.v4();
+    const nodeVersion =
+      typeScriptConf.get('javascript.node.version') ?? 'latest';
+    const nodeMajorVersion =
+      nodeVersion === 'latest'
+        ? DEFAULT_NODE_MAJOR_VERSION
+        : major(nodeVersion).toString();
     const buildArgs: Record<string, string> = {
-      NODE_VERSION: typeScriptConf.get('javascript.node.version') ?? 'latest',
+      NODE_VERSION: nodeVersion,
+      NODE_MAJOR_VERSION: nodeMajorVersion,
       NPM_VERSION: typeScriptConf.get('javascript.npm.version') ?? 'latest',
       ...(await serviceContainerConf.getAndRender(
         'serviceContainer.buildArgs',
