a8b5e085caea0caaee15d9f76f96982844bc4e0c
Flo <florent.vouin@gmail.com>
2023-10-31 18:29:25 +0100

    ✨ Implement the getSingleType utility

--- src/code-generation/utilities.ts
+++ src/code-generation/utilities.ts
@@ -1,4 +1,5 @@
-import { Sourcelike } from 'quicktype-core';
+import { ArrayType, Sourcelike, Type } from 'quicktype-core';
+import { removeNullFromType } from 'quicktype-core/dist/TypeUtils.js';
 
 /**
  * Constructs the TypeScript source code for the given object.
@@ -29,4 +30,42 @@ export function typeScriptSourceForObject(
   );
 
   return sourceOptions.length > 0 ? ['{ ', ...sourceOptions, '}'] : '';
+}
+
+/**
+ * Removes the null type from the given type, and extracts the items type for array types.
+ *
+ * @param type The type for which the single type should be extracted.
+ * @returns Information about the single type, or `null` if the type is not a single type.
+ */
+export function getSingleType(type: Type): {
+  /**
+   * The single type.
+   */
+  type: Type;
+
+  /**
+   * Whether the original type was nullable.
+   */
+  isNullable: boolean;
+
+  /**
+   * Whether the original type was an array.
+   */
+  isArray: boolean;
+} | null {
+  const [nullType, propertyType] = removeNullFromType(type);
+  if (propertyType.size !== 1) {
+    return null;
+  }
+
+  const isNullable = nullType !== null;
+  const nonNullType = [...propertyType][0];
+  const isArray = nonNullType.kind === 'array';
+
+  if (isArray) {
+    return { type: (nonNullType as ArrayType).items, isNullable, isArray };
+  }
+
+  return { type: nonNullType, isNullable, isArray };
 }
